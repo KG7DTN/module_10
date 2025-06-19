1b7795b581b0adc761c537cf4c4ca1a1ce44fd0a
Chris Richins <christopher.j.richins@gmail.com>
2025-06-18 23:21:36 -0700

    Rough draft completion. Some final fixes needed

--- html_render.py
+++ html_render.py
@@ -7,6 +7,7 @@ A class-based system for rendering html.
 
 # This is the framework for the base class
 class Element:
+    '''Base Element Class which contains append and render'''
 
     tag = 'html'
     indent = '    '
@@ -16,9 +17,11 @@ class Element:
         self.attributes = kwargs
 
     def append(self, new_content):
+        '''appends new content to the page contents'''
         self.contents.append(new_content)
 
     def render(self, out_file, ind=""):
+        '''base content rendering'''
         if not self.attributes:
             out_file.write(f"{ind}<{self.tag}>\n")
         else:
@@ -33,6 +36,7 @@ class Element:
         out_file.write(f"{ind}</{self.tag}>\n")
 
 class Html(Element):
+    '''html tag handling'''
     tag = 'html'
 
     def render(self, out_file, ind=""):
@@ -41,15 +45,19 @@ class Html(Element):
 
 
 class Body(Element):
+    '''Body tag handling'''
     tag = 'body'
 
 class P(Element):
+    '''paragraph tag handling'''
     tag = 'p'
 
 class Head(Element):
+    '''head tag handling'''
     tag = 'head'
 
 class SelfClosingTag(Element):
+    '''self closing tags'''
     def __init__(self, **kwargs):
         super().__init__(content=None, **kwargs)
 
@@ -60,13 +68,20 @@ class SelfClosingTag(Element):
         else:
             out_file.write(f"{ind}<{self.tag} />\n")
 
+class Meta(Element):
+    '''meta tag handling'''
+    tag = 'meta'
+
 class Br(SelfClosingTag):
+    '''Break tag Handling'''
     tag = "br"
 
 class Hr(SelfClosingTag):
+    '''header tag handling'''
     tag = "hr"
 
 class OneLineTag(Element):
+    '''one liner handling'''
     def render(self, out_file, ind=""):
         attr_str = ''.join(f' {key}="{value}"' for key, value in self.attributes.items())
         out_file.write(f"{ind}<{self.tag}{attr_str}>")
@@ -75,10 +90,28 @@ class OneLineTag(Element):
         out_file.write(f"</{self.tag}>\n")
 
 class Title(OneLineTag):
+    '''Title tag handling'''
     tag = 'title'
 
 class A(OneLineTag):
+    '''attribute handling for links'''
     tag = 'a'
 
     def __init__(self, link, text):
         super().__init__(text, href=link)
+
+class H(OneLineTag):
+    '''header and level tag handling'''
+    tag = 'h'
+
+    def __init__(self, level, content):
+        self.tag = f'h{level}'
+        super().__init__(content)
+
+class Ul(Element):
+    '''unstructured list handling'''
+    tag = 'ul'
+
+class Li(Ul):
+    '''list handling'''
+    tag = 'li'
